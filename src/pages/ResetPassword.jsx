04ebe8f97e087eea5a10008db0a50b0ab4ddfa11
Gevorg Shahnazaryan <bboygevor@gmail.com>
2023-06-21 03:15:12 +0400

    update admin panel

---
+++ src/pages/ResetPassword.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import CustomInput from "../components/CustomInput";
+
+const ResetPassword = () => {
+  return (
+    <div className="py-5" style={{ background: "#ffd333", minHeight: "100vh" }}>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className="my-5 w-25 bg-white roundet-3 mx-auto p-4">
+        <h3 className="text-center title">Login</h3>
+        <p className="text-center">Login to your account to continue</p>
+        <form action="">
+          <CustomInput type="text" label="Email Address" id="email" />
+          <CustomInput type="password" label="Password" id="pass" />
+
+          <button
+            className="border-0 px-3 py-2 text-white fw-bold w-100"
+            style={{ background: "#ffd333" }}
+            type="submit"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ResetPassword;
